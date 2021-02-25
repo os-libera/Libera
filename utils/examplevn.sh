4212420ed26b1e849d0ad2eaf9edb7f5283b7f41
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2021-02-25 18:08:20 +0900

    Update examplevn.sh

--- utils/examplevn.sh
+++ utils/examplevn.sh
@@ -12,3 +12,4 @@ python ovxctl.py -n connectLink 1 00:a4:23:05:00:00:00:01 2 00:a4:23:05:00:00:00
 python ovxctl.py -n connectLink 1 00:a4:23:05:00:00:00:02 2 00:a4:23:05:00:00:00:03 1 spf 1
 python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:01 1 00:00:00:00:01:01
 python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:03 2 00:00:00:00:03:02
+python ovxctl.py -n startNetwork 1
